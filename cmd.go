5a7e00a8dcf2060b651f90f8b7285f29f9569e78
Rob Muhlestein <rob@rwx.gg>
2022-12-02 01:56:53 -0500

    Update version

--- cmd.go
+++ cmd.go
@@ -89,7 +89,7 @@ var Cmd = &Z.Cmd{
 	Name:      `keg`,
 	Aliases:   []string{`kn`},
 	Summary:   `create and manage knowledge exchange graphs`,
-	Version:   `v0.6.1`,
+	Version:   `v0.7.0`,
 	UseVars:   true,
 	Copyright: `Copyright 2022 Robert S Muhlestein`,
 	License:   `Apache-2.0`,

cbfdf165f5eca16d86a1c128b0d5a84d73370c67
Rob Muhlestein <rob@rwx.gg>
2022-12-05 23:09:06 -0500

    Add help for grep (closes #93)

--- cmd.go
+++ cmd.go
@@ -871,10 +871,12 @@ var grepCmd = &Z.Cmd{
 	Commands: []*Z.Cmd{help.Cmd},
 
 	Description: `
-		The {{aka}} performs a simple regular expression grep of all node
-		README.md files. (Does not depend on host {{pre "grep"}} command. By
-		default, all regular expressions are case-sensitive (unlike default
-		{{cmd "title"}} or {{cmd "edit"}} commands).  This can be
+		The {{aka}} command performs a simple regular expression search of
+		all node README.md files. It does not depend on any {{pre "grep"}}
+		command being installed.
+		
+		By default, all regular expressions are case-sensitive (unlike
+		default {{cmd "title"}} or {{cmd "edit"}} commands). This can be
 		explicitly overridden for a given search by adding {{pre "(?i)"}} or
 		for all searches by setting the global {{pre "regxpre"}} variable to
 		the same:
@@ -883,6 +885,13 @@ var grepCmd = &Z.Cmd{
 		
 		Note that if set, {{pre "regxpre"}} applies to *all* searches, which
 		includes the {{cmd "titles"}} and {{cmd "edit"}} commands.
+		
+		When run interactively the choice of hits is presented so that the
+		user may select. The selection is then delegated to the {{cmd
+		"edit"}} command.
+		
+		When run non-interactively the matching files are listed with their
+		titles as a node include list.
 		
 		`,
 
